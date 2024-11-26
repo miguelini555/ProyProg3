a51fc5cd265984fb532f46062e85ecc07a41bba0
miguelini555 <guevarsarmientoemanuel4@gmail.com>
2024-11-26 08:48:06 -0400

    update target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.cumbiacoders"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.cumbiacoders"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
