f9b58ba6a64d17e0f3f6a0bc273198f4296386b6
Nahomi Humerez <146476879+TrynaFindMimi@users.noreply.github.com>
2024-12-10 11:53:40 -0400

    nahomi timer update, and binding to home

--- app/src/main/java/com/example/cumbiacoders/HomeActivity.kt
+++ app/src/main/java/com/example/cumbiacoders/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.cumbiacoders
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -24,6 +25,8 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.startActivityButtonHome.setOnClickListener {
+            val intent = Intent(this,TimerActivity::class.java)
+            startActivity(intent)
 
         }
         binding.profileButtonHome.setOnClickListener {
