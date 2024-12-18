5ee1d8cb0e01efbde1c91b5fb783563678f26ad0
Nahomi Humerez <146476879+TrynaFindMimi@users.noreply.github.com>
2024-12-17 23:02:11 -0400

    Habit History Logica

--- app/src/main/java/com/example/cumbiacoders/HabitHistoryActivity.kt
+++ app/src/main/java/com/example/cumbiacoders/HabitHistoryActivity.kt
@@ -1,20 +1,51 @@
 package com.example.cumbiacoders
 
+import android.content.Context
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cumbiacoders.adapters.HabitAdapter
+import com.example.cumbiacoders.dataClases.Habit
+import com.example.cumbiacoders.databinding.ActivityHabitHistoryBinding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class HabitHistoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHabitHistoryBinding
+    private lateinit var historyAdapter: HabitAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_habit_history)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityHabitHistoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpRecyclerViews()
+        loadHistory()
+    }
+
+    private fun setUpRecyclerViews() {
+        historyAdapter = HabitAdapter(mutableListOf()) { habit ->
+
+        }
+        binding.habitHistoryRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this@HabitHistoryActivity)
+            adapter = historyAdapter
+        }
+    }
+
+    private fun loadHistory() {
+        val sharedPreferences = getSharedPreferences("habits_prefs", Context.MODE_PRIVATE)
+        val json = sharedPreferences.getString("habits", null)
+
+        if (!json.isNullOrEmpty()) {
+            val type = object : TypeToken<List<Habit>>() {}.type
+            val habits = Gson().fromJson<List<Habit>>(json, type)
+
+            historyAdapter.actualizarLista(habits)
+        } else {
+            historyAdapter.actualizarLista(emptyList())
         }
     }
 }
